efe94fbe2f327a9081db559b80c71a799fecb216
nelu572 <s25031@gsm.hs.kr>
2026-08-19 14:49:54 +0900

    fix(Player): Camera Pivot 탐색 추가

--- Assets/Scripts/Player/PlayerRotate.cs
+++ Assets/Scripts/Player/PlayerRotate.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 
 public class PlayerRotate : MonoBehaviour
 {
-    [SerializeField] private Transform CameraPivotTransfotm;
-    [SerializeField] private Transform PlayerTransform;
+    [SerializeField] private Transform CameraPivotTransform;
+
+    void Start()
+    {
+        CameraPivotTransform ??= Camera.main.transform.parent.transform;
+    }
 
     public void LookMouse(Vector2 mouseDelta)
     {
-        float xRotation = CameraPivotTransfotm.localRotation.eulerAngles.x;
+        float xRotation = CameraPivotTransform.localRotation.eulerAngles.x;
         if (xRotation > 180f) xRotation -= 360f;
 
         float targetX = xRotation - (mouseDelta.y * Constant.Mouse.LOOK_SENSITIVITY);
         targetX = Mathf.Clamp(targetX, -85f, 85f);
 
-        CameraPivotTransfotm.localRotation = Quaternion.Euler(targetX, 0f, 0f);
+        CameraPivotTransform.localRotation = Quaternion.Euler(targetX, 0f, 0f);
 
-        PlayerTransform.Rotate(Vector3.up * mouseDelta.x * Constant.Mouse.LOOK_SENSITIVITY);
+        transform.Rotate(Vector3.up * mouseDelta.x * Constant.Mouse.LOOK_SENSITIVITY);
     }
 }
